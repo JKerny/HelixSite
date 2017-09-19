f29890eeadaad73f611a8a29b05545da4763f5f5
HOME\Coolio <jkerny81@gmail.com>
2017-09-19 21:37:54 +1000

    Initial Commit

---
+++ gulp-config.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+    var instanceRoot = "C:\Repos\HelixSite\HelixSite\SandBox";
+  var config = {
+    websiteRoot: instanceRoot + "\\Website",
+    sitecoreLibraries: instanceRoot + "\\Website\\bin",
+    licensePath: instanceRoot + "\\Data\\license.xml",
+    solutionName: "Habitat",
+    buildConfiguration: "Debug",
+    runCleanBuilds: false
+  };
+  return config;
+}
