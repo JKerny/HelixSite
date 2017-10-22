2aaa161cb1c9cbfeb153a1100bca51035acd4483
HOME\Coolio <jkerny81@gmail.com>
2017-10-22 21:43:50 +1100

    re factoring

---
+++ src/Foundation/DependancyInjection/code/Infrastructure/IocContainerConfigurator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using Glass.Mapper.Sc;
+
+namespace HelixSite.Foundation.DependencyInjection.Infrastructure
+{
+    public class IocContainerConfigurator : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped(typeof(ISitecoreContext), typeof(SitecoreContext));
+            serviceCollection.AddMvcControllers("*.Feature.*");
+        }
+    }
+}
